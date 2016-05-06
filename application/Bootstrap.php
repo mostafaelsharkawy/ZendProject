19db07c0f1010fa6e4e93b1100ad4c3061307f87
ahmadSaeedGoda <ahmad.Saeed.Goda@gmail.com>
2016-05-06 06:47:37 +0200

    first commit

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,25 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+	protected function _initSession(){
+		Zend_Session::start();
+		$session = new Zend_Session_Namespace( 'Zend_Auth' );
+		//$session->setExpirationSeconds( 1800 );
+	}
 
-
+	protected function _initPlaceholders()
+	{
+		$this->bootstrap('View');
+		$view = $this->getResource('View');
+		$view->doctype('XHTML1_STRICT');
+//Meta
+		$view->headMeta()->appendName('keywords', 'framework, PHP')->appendHttpEquiv('Content-Type','text/html;charset=utf-8');
+// Set the initial title and separator:
+		$view->headTitle('OS Site')->setSeparator(' :: ');
+// Set the initial stylesheet:
+		$view->headLink()->prependStylesheet('http://localhost/zend_test/public/css/bootstrap.css');
+		$view->headLink()->prependStylesheet('http://localhost/zend_test/public/css/style.css');
+// Set the initial JS to load:
+		// $view->headScript()->prependFile('/js/site.js');
+	}
 }
-
