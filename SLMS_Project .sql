4bb87c01475001e5e9b7265a6b2010b1227b32b2
engsamar <eng.samar.2014@gmail.com>
2016-05-07 21:21:26 +0200

    added model crud

--- SLMS_Project .sql
+++ SLMS_Project .sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.4.13.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 06, 2016 at 04:49 PM
--- Server version: 5.5.49-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.16
+-- Generation Time: May 07, 2016 at 06:51 PM
+-- Server version: 5.6.28-0ubuntu0.15.10.1
+-- PHP Version: 5.6.11-1ubuntu3.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `SLMS_Project`
@@ -27,14 +27,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `comments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `content` varchar(150) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `material_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  KEY `material_id` (`material_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `material_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -43,14 +40,11 @@ CREATE TABLE IF NOT EXISTS `comments` (
 --
 
 CREATE TABLE IF NOT EXISTS `courses` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `title` varchar(150) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `parent_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  KEY `parent_id` (`parent_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `parent_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -59,17 +53,15 @@ CREATE TABLE IF NOT EXISTS `courses` (
 --
 
 CREATE TABLE IF NOT EXISTS `materials` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `title` varchar(150) NOT NULL,
+  `material_type_id` int(11) NOT NULL,
   `is_approved` tinyint(1) NOT NULL,
   `is_published` tinyint(1) NOT NULL,
   `is_shown` tinyint(1) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `course_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  KEY `course_id` (`course_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `course_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -78,14 +70,10 @@ CREATE TABLE IF NOT EXISTS `materials` (
 --
 
 CREATE TABLE IF NOT EXISTS `material_contents` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `content_path` varchar(250) NOT NULL,
-  `material_type_id` int(11) NOT NULL,
-  `material_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `material_type_id` (`material_type_id`),
-  KEY `material_id` (`material_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `material_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -94,10 +82,10 @@ CREATE TABLE IF NOT EXISTS `material_contents` (
 --
 
 CREATE TABLE IF NOT EXISTS `material_types` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `type` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -106,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `material_types` (
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` char(32) NOT NULL,
   `email` varchar(100) NOT NULL,
@@ -115,9 +103,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `signature` varchar(50) NOT NULL,
   `photo` varchar(250) NOT NULL,
   `is_Admin` tinyint(1) NOT NULL,
-  `is_Banned` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `is_Banned` tinyint(1) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -127,6 +114,87 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`, `gender`, `country`,
 (1, 'yasmin', '', '', '', '', '', '', 0, 0),
 (2, 'samar', '', '', '', '', '', '', 0, 0);
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `material_id` (`material_id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `parent_id` (`parent_id`);
+
+--
+-- Indexes for table `materials`
+--
+ALTER TABLE `materials`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `material_contents`
+--
+ALTER TABLE `material_contents`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `material_id` (`material_id`);
+
+--
+-- Indexes for table `material_types`
+--
+ALTER TABLE `material_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `materials`
+--
+ALTER TABLE `materials`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `material_contents`
+--
+ALTER TABLE `material_contents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `material_types`
+--
+ALTER TABLE `material_types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
@@ -156,7 +224,6 @@ ALTER TABLE `materials`
 -- Constraints for table `material_contents`
 --
 ALTER TABLE `material_contents`
-  ADD CONSTRAINT `material_contents_ibfk_1` FOREIGN KEY (`material_type_id`) REFERENCES `material_types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `material_contents_ibfk_2` FOREIGN KEY (`material_id`) REFERENCES `materials` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- application/models/DbTable/Course.php
+++ application/models/DbTable/Course.php
@@ -4,6 +4,43 @@ class Application_Model_DbTable_Course extends Zend_Db_Table_Abstract
 {
 
     protected $_name = 'courses';
+    function getCategories(){
+		return $this->fetchAll($this->select()->where('parent_id=?',"NULL"));
+	}
+
+	function editCategories($id,$categoryInfo){
+		$this->update($categoryInfo, "id=$id");
+	}
+	
+	function deleteCategory($id){
+		return $this->delete('id='.$id);
+	}
+	function addCategory($categoryInfo){
+		$row = $this->createRow();
+		$row->title = $categoryInfo['title'];
+		$row->user_id = $categoryInfo['user_id'];
+		$row->parent_id = "NULL";
+		return $row->save();
+	}
+
+	function getCourses(){
+		return $this->fetchAll($this->select()->where('parent_id !=?',"NULL"));
+	}
+
+	function editCourses($id,$coursesInfo){
+		$this->update($coursesInfo, "id=$id");
+	}
+	
+	function deleteCourses($id){
+		return $this->delete('id='.$id);
+	}
+	function addCourses($coursesInfo){
+		$row = $this->createRow();
+		$row->content = $coursesInfo['content'];
+		$row->user_id = $coursesInfo['user_id'];
+		$row->parent_id = $coursesInfo['parent_id'];
+		return $row->save();
+	}
 
 
 }
