dd167095986a1dc899b8a03de527bc90587a7350
engsamar <eng.samar.2014@gmail.com>
2016-05-08 21:13:03 +0200

    models

--- application/models/DbTable/MaterialContent.php
+++ application/models/DbTable/MaterialContent.php
@@ -4,7 +4,5 @@ class Application_Model_DbTable_MaterialContent extends Zend_Db_Table_Abstract
 {
 
     protected $_name = 'material_contents';
-
-
 }
 
--- application/models/DbTable/User.php
+++ application/models/DbTable/User.php
@@ -3,7 +3,36 @@
 class Application_Model_DbTable_User extends Zend_Db_Table_Abstract
 {
 
-    protected $_name = 'users';
+    protected $_name = 'users';	
+
+	function listUsers(){
+		return $this->fetchAll()->toArray();
+	}
+	
+	function getUserById($id){
+		return $this->find($id)->toArray();
+	}
+	function editUser($id,$userInfo){
+		$this->update($userInfo, "id=$id");
+	}
+	
+
+	function deleteUser($id){
+		return $this->delete('id='.$id);
+	}
+	function addUser($userInfo){
+		$row = $this->createRow();
+		$row->username = $userInfo['username'];
+		$row->password = md5($userInfo['password']);
+		$row->email = $userInfo['email'];
+		$row->gender = $userInfo['gender'];
+		$row->country = $userInfo['country'];
+		$row->is_admin = '0';
+		$row->is_Banned = '0';
+
+	return $row->save();
+	}
+
 
 
 }
