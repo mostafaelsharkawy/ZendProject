51ff7c4c204ab00ba7f1199c7ccd3a339d7a7c94
engsamar <eng.samar.2014@gmail.com>
2016-05-13 15:30:58 +0200

    updating front end layout

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -32,15 +32,37 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
         $view->headLink()->prependStylesheet('/ZendProject/public/css/bootstrap.min.css');
         // $view->headLink()->prependStylesheet('/ZendProject/public/css/bootstrap-rtl.css');
         // $view->headLink()->prependStylesheet('/ZendProject/public/css/bootstrap-rtl.min.css');
-        $view->headLink()->appendStylesheet('/ZendProject/public/css/sb-admin.css');
+        //$view->headLink()->appendStylesheet('/ZendProject/public/css/sb-admin.css');
         // $view->headLink()->prependStylesheet('/ZendProject/public/css/sb-admin-rtl.css');
         $view->headLink()->appendStylesheet('/ZendProject/public/css/plugins/morris.css');
         $view->headLink()->appendStylesheet('/ZendProject/public/font-awesome/css/font-awesome.min.css');
+        
+        $view->headLink()->appendStylesheet('/ZendProject/public/css/animate.css');
+     //   $view->headLink()->appendStylesheet('/ZendProject/public/css/queryLoader.css');
+        $view->headLink()->appendStylesheet('/ZendProject/public/css/superslides.css');
+       // $view->headLink()->appendStylesheet('/ZendProject/public/css/slick.css');
+        
+        
+        $view->headLink()->appendStylesheet('/ZendProject/public/css/jquery.tosrus.all.css');
+        $view->headLink()->appendStylesheet('/ZendProject/public/css/themes/default-theme.css');
+        $view->headLink()->appendStylesheet('/ZendProject/public/css/style.css');
+        
+        
         // Set the initial JS to load:
         // $view->headScript()->prependFile('/ZendProject/public/js/bootstrap.js');
         $view->headScript()->appendFile('/ZendProject/public/js/jquery.js');
         $view->headScript()->appendFile('/ZendProject/public/js/jquery-2.1.4.min.js');
         $view->headScript()->appendFile('/ZendProject/public/js/bootstrap.min.js');
+        
+        $view->headScript()->appendFile('/ZendProject/public/js/queryloader2.min.js');
+        $view->headScript()->appendFile('/ZendProject/public/js/wow.min.js');
+        $view->headScript()->appendFile('/ZendProject/public/js/slick.min.js');
+        $view->headScript()->appendFile('/ZendProject/public/js/jquery.tosrus.min.all.js');
+        $view->headScript()->appendFile('/ZendProject/public/js/custom.js');
+        $view->headScript()->appendFile('/ZendProject/public/js/jquery.easing.1.3.js');
+        $view->headScript()->appendFile('/ZendProject/public/js/jquery.animate-enhanced.min.js');
+        $view->headScript()->appendFile('/ZendProject/public/js/jquery.superslides.min.js');
+        
         // $view->headScript()->prependFile('/ZendProject/public/js/plugins/flot/excanvas.min.js');
         // $view->headScript()->prependFile('/ZendProject/public/js/plugins/flot/flot-data.js');
         // $view->headScript()->prependFile('/ZendProject/public/js/plugins/flot/jquery.flot.js');
